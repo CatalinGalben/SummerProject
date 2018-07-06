5a4ddb8cb4553955d2b43dbacd41b49bc559adaf
Galben <catalin.galben.ext@siemens.com>
2018-07-06 10:33:51 +0300

    yahoo finance

--- src/main/java/asdas.java
+++ src/main/java/asdas.java
@@ -1,11 +1,7 @@
-import org.patriques.AlphaVantageConnector;
-import org.patriques.TimeSeries;
-import org.patriques.input.timeseries.Interval;
-import org.patriques.input.timeseries.OutputSize;
-import org.patriques.output.AlphaVantageException;
-import org.patriques.output.timeseries.IntraDay;
-import org.patriques.output.timeseries.data.StockData;
+import yahoofinance.Stock;
+import yahoofinance.YahooFinance;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Map;
 
@@ -13,29 +9,19 @@ public class asdas {
     public static void main(String[] args){
         String key = "5G9AEN06TMIBRAAS";
         int timeout = 3000;
-
-        AlphaVantageConnector apiConnector = new AlphaVantageConnector(key, timeout);
-        TimeSeries stockTimeSeries = new TimeSeries(apiConnector);
-
         try{
-            IntraDay response = stockTimeSeries.intraDay("MSFT", Interval.ONE_MIN, OutputSize.COMPACT);
-            Map<String, String> metaData = response.getMetaData();
-            System.out.println("Information: "+metaData.get("1. Information"));
-            System.out.println("Stock" + metaData.get("2. Symbol"));
-            List<StockData> stockData = response.getStockData();
-            stockData.forEach(
-                    stock -> {
-                        System.out.println("date:   " + stock.getDateTime());
-                        System.out.println("open:   " + stock.getOpen());
-                        System.out.println("high:   " + stock.getHigh());
-                        System.out.println("low:   " + stock.getLow());
-                        System.out.println("close:   "+stock.getClose());
-                        System.out.println("volume:   " + stock.getVolume());
+            Stock stock = YahooFinance.get("SIE.DE");
+            BigDecimal price = stock.getQuote().getPrice();
+            BigDecimal change = stock.getQuote().getChangeInPercent();
+            BigDecimal peg = stock.getStats().getPeg();
+            BigDecimal dividend = stock.getDividend().getAnnualYieldPercent();
 
-                    }
-            );
-        }catch (AlphaVantageException e){
-            System.out.println("something went wrong");
+            stock.print();
+        }catch(Exception e)
+        {
+            System.out.println(e);
         }
+
+
     }
 }
