740ffd77f0310cc814f79f7078ca5a78a0ba95d3
Galben <catalin.galben.ext@siemens.com>
2018-07-12 10:28:13 +0300

    configurations

---
+++ web/src/main/java/com/siemens/web/config/AppLocalConfig.java
@@ -0,0 +1,18 @@
+package com.siemens.web.config;
+
+import com.siemens.core.config.JPAConfig;
+import org.springframework.context.annotation.*;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+
+@Configuration
+@ComponentScan({"com.siemens.core"})
+@Import({JPAConfig.class})
+@PropertySources({@PropertySource(value = "classpath:local/database.properties")})
+public class AppLocalConfig {
+   @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer()
+   {
+       return new PropertySourcesPlaceholderConfigurer();
+   }
+}
---
+++ web/src/main/java/com/siemens/web/config/WebConfig.java
@@ -0,0 +1,28 @@
+package com.siemens.web.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan({"com.siemens.web.controller", "com.siemens.web.converter"})
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer()
+    {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost4200", "http://localhost:8080")
+                        .allowedMethods("GET", "PUT,","POST","DELETE");
+            }
+        };
+    }
+}
+
