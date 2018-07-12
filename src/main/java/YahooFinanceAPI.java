9be7c062adf1e8ebddc48faa051a22a24f2c2f65
Galben <catalin.galben.ext@siemens.com>
2018-07-12 09:04:34 +0300

    file structure

--- src/main/java/YahooFinanceAPI.java
+++ src/main/java/YahooFinanceAPI.java
@@ -5,16 +5,15 @@ import java.math.BigDecimal;
 public class YahooFinanceAPI {
     public static void main(String[] args){
         try{
-            Stock stock = YahooFinance.get("NOK");
+            Stock stock = YahooFinance.get("SIE.DE");
             BigDecimal price = stock.getQuote().getPrice();
             BigDecimal change = stock.getQuote().getChangeInPercent();
             BigDecimal dividend = stock.getDividend().getAnnualYield();
             BigDecimal pe = stock.getStats().getPe();
 
-
             //This should be used if you want to print all the details provided
             stock.print();
-/*            System.out.println(stock.getName());
+/*          System.out.println(stock.getName());
             System.out.println("Price: " + price);
             System.out.println("Change: " + change);
             System.out.println("Dividend Yield: " + dividend);
