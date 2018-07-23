458ea950ac567b79323d563f5d65bd4e773272a3
Galben <catalin.galben.ext@siemens.com>
2018-07-23 15:31:23 +0300

    groups + benchmarks backend + solved etf/trust types

---
+++ core/src/main/java/com/siemens/core/service/GroupServiceInterface.java
@@ -0,0 +1,25 @@
+package com.siemens.core.service;
+
+import com.siemens.core.model.Company;
+import com.siemens.core.model.CompanyGroup;
+import com.siemens.core.model.Group;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public interface GroupServiceInterface {
+
+    void createSubGroup(Set<Company> companies, String nameOfGroup, Group parentGroup); // determine parent ?
+
+    void createGroup(Set<Company> companies, String nameOfGroups);
+
+
+    Map<String, String> getBenchmarks(Set<Group> groups);
+
+    List<Group> getGroups();
+
+    List<CompanyGroup> getCompanyGroups();
+
+}
