de123253753b6106f65e44997e5ebd274ed58d87
Prodan <andreea.prodan.ext@siemens.com>
2018-07-24 09:59:56 +0300

    front-end legend to chart and sort option to portfolio

--- web/src/main/webapp/src/app/portfolio/portfolio.component.ts
+++ web/src/main/webapp/src/app/portfolio/portfolio.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { RECORDS } from '../mock-data';
 import {Router} from "@angular/router";
 import {TransferService} from "../providers/transfer.service";
+import {Record} from "../record";
+import {v} from "@angular/core/src/render3";
 
 @Component({
   selector: 'app-portfolio',
@@ -28,5 +30,26 @@ export class PortfolioComponent implements OnInit {
 
     this.selectedRow = name;
   }
+
+
+  insertionSort(rec: Record[], n:number)
+  {
+    let i, key, j;
+    for (i = 1; i < n; i++)
+    {
+      key = this.records[i];
+      j = i-1;
+
+      /* Move elements of arr[0..i-1], that are
+         greater than key, to one position ahead
+         of their current position */
+      while (j >= 0 && this.records[j].name > key.name)
+      {
+        this.records[j+1] = this.records[j];
+        j = j-1;
+      }
+      this.records[j+1] = key;
+      }
+    }
 
 }
