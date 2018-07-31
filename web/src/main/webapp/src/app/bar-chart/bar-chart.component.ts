8b3b96385e49879d40483ff10f0e19c9c761f905
Prodan <andreea.prodan.ext@siemens.com>
2018-07-31 16:56:23 +0300

    front-end display groups in the chart page

--- web/src/main/webapp/src/app/bar-chart/bar-chart.component.ts
+++ web/src/main/webapp/src/app/bar-chart/bar-chart.component.ts
@@ -1,20 +1,48 @@
 import { Component, OnInit, OnChanges, Input } from '@angular/core';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
 import {ChartService} from "./shared/chart.service";
+import {Group} from "./shared/Group.model";
+import {LoginService} from "../login-page/shared/login.service";
+import {User} from "../login-page/shared/user.model";
+import {HoldingRecord} from "../add-record/shared/HoldingRecord.model";
+import {PortfolioService} from "../portfolio/shared/portfolio.service";
+import {Company} from "../add-record/shared/Company.model";
+import {AddRecordService} from "../add-record/shared/add-record.service";
+import {CompanyGroup} from "./shared/CompanyGroup.model";
+import {SharePrice} from "../add-record/shared/SharePrice.model";
+import {Broker} from "../add-record/shared/Broker.model";
 
 @Component({
   selector: 'app-ngx-charts',
   templateUrl: './bar-chart.component.html',
   styleUrls: ['./bar-chart.component.css']
 })
-export class BarChartComponent implements OnInit, OnChanges {
+export class BarChartComponent implements OnInit {
   @Input() commodity: any;
   results: any;
 
+  results2: any;
 
-  constructor(private chartService: ChartService) { }
+  selectedRow: any;
+
+  userLoggedIn: User;
+  companies: Company[] = [];
+  companyGroups: CompanyGroup[] = [];
+
+  holdingRecords: HoldingRecord[] = [];
+  groups: Group[] = [];
+
+
+  constructor(private groupService: ChartService,
+              private loginService: LoginService,
+              private portfolioService: PortfolioService,
+              private recordService: AddRecordService) {
+
+    this.populate();
+  }
 
   ngOnInit() {
+    this.results2 = [];
     this.results = [
       {
         "name": "Isle of Man",
@@ -143,13 +171,189 @@ export class BarChartComponent implements OnInit, OnChanges {
       }
     ]
 
+    /*
+    console.log("BC: " + this.results[0].name);
+    console.log("BC: " + this.results[0].series[0].value);
+    console.log("BC: " + this.results[0].series[0].name);
+
+    for(let i=0; i<this.results.length; i++) {
+      console.log(this.results[i].name);
+      for(let j=0; j<this.results.length; j++) {
+        console.log(this.results[i].series[j].value);
+        console.log(this.results[i].series[j].name);
+      }
+    }
+*/
+
+    /*
+    const index = this.checkinTemp.indexOf(item);
+    this.checkinTemp.splice(index, 1);
+     */
+    this.results.splice(3,1); // delete Suriname
+
+    this.results.push( {  // add new node
+      "name": "Trrrrrr",
+      "series": [
+        {
+          "value": 6918,
+          "name": "2016-09-16T16:58:17.749Z"
+        },
+        {
+          "value": 3183,
+          "name": "2016-09-23T03:15:22.044Z"
+        },
+        {
+          "value": 5903,
+          "name": "2016-09-21T18:51:48.246Z"
+        },
+        {
+          "value": 6019,
+          "name": "2016-09-18T20:29:50.869Z"
+        },
+        {
+          "value": 4940,
+          "name": "2016-09-14T04:06:26.043Z"
+        }
+      ]
+    });
+
+    this.results2.push({  // add new node
+      "name": "Trrrrrr",
+      "series": [
+        {
+          "value": 6918,
+          "name": "2016-09-16T16:58:17.749Z"
+        },
+        {
+          "value": 3183,
+          "name": "2016-09-23T03:15:22.044Z"
+        },
+        {
+          "value": 5903,
+          "name": "2016-09-21T18:51:48.246Z"
+        },
+        {
+          "value": 6019,
+          "name": "2016-09-18T20:29:50.869Z"
+        },
+        {
+          "value": 4940,
+          "name": "2016-09-14T04:06:26.043Z"
+        }
+      ]
+    });
+  }
+
+  populate() {
+    if (this.loginService.getCurrentUser() != null) {
+        // get user data
+        this.userLoggedIn = this.loginService.getCurrentUser();
 
 
+        // get all records for the user
+        this.portfolioService.getRecords().subscribe(records => {
+          console.log(records);
+          this.holdingRecords = records;
+          this.holdingRecords.filter(hr => hr.userid == this.userLoggedIn.id)
+        });
+        console.log(this.holdingRecords);
 
+        // get all the companies groups of the user
+        this.recordService.getAllCompanies().subscribe(records => {
+            this.companies = records;
+            this.companies.filter(comp => {
+                 for (let i = 0; i < this.holdingRecords.length; i++)
+                   if (this.holdingRecords[i].companyid == comp.id)
+                      return false;
+                 return true;
+               } );
+         });
+
+        // get all CompanyGroups of the user
+        this.groupService.getCompanyGroups().subscribe(records => {
+            this.companyGroups = records;
+            this.companyGroups.filter(comp => {
+              for (let i = 0; i < this.holdingRecords.length; i++)
+                if (this.holdingRecords[i].companyid == comp.companyid)
+                  return false;
+              return true;
+            } );
+        });
+
+
+      // get all user's groups
+      this.groupService.getGroups().subscribe( records => {
+        this.groups = records;
+        this.groups.filter(gr => {
+            for (let i = 0; i < this.companyGroups.length; i++) {
+              if (this.companyGroups[i].groupid == gr.id)
+                return false;
+            }
+            return true;
+        });
+      });
+    }
+
+    //get all sharePrices
+    this.recordService.getAllSharePrices().subscribe(shareprices =>{
+      this.sharePrices = shareprices.sort((s1, s2)=> s1.id-s2.id);
+      this.loginService.changeSharePrices(this.sharePrices);});
+
+    //get all brokers
+    this.recordService.getAllBrokers().subscribe(brokers => {
+      this.brokers = brokers;
+      this.loginService.changeBrokers(this.brokers);
+    });
 
   }
 
-  ngOnChanges() {
+  sharePrices: SharePrice[] = [];
+  currentCompanyNamePortfolio: string;
+  brokers: Broker[] = [];
+
+  getCompanyForRecord(id: number, rec: HoldingRecord): string{
+    this.currentCompanyNamePortfolio = this.companies.filter(company => company.id == id)[0].name;
+    return this.currentCompanyNamePortfolio
+  }
+
+  getCurrentShareForCompany(companyid: number): number{
+    return this.sharePrices.filter(shareprice => shareprice.companyid == companyid)[0].price;
+  }
+
+  getGain(paid: number, sumNow: number): number{
+    let result = paid/sumNow;
+    if (result >= 1){
+      return (result - 1)*100;
+    }
+    else
+      return (1 - result)*100;
+  }
+
+  getBrokerForRecord(brokerid: number): string {
+    return this.brokers.filter(broker => broker.id == brokerid)[0].name;
+  }
+
+  clickRecord(name: string) {
+    this.selectedRow = name;
+    //this.addNode(name);
+
+    console.log("NN " + this.selectedRow);
+
+    console.log(this.userLoggedIn);
+    console.log("HR: ");
+    console.log(this.holdingRecords);
+    console.log("Companies: ");
+    console.log(this.companies);
+    console.log("Company Groups: ");
+    console.log(this.companyGroups)
+    console.log("Groups: ");
+    console.log(this.groups)
+  }
+
+  addNode(name: string) {
+    let node = this.results.find(x => x.name == name);
+    console.log(node);
+    this.results2.push(node);
   }
 
 }
